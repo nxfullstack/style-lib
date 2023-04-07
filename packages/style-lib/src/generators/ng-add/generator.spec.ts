5d0597c2ef3a9607605efb9be3aa0b17ff745036
wgd3 <wallace@thefullstack.engineer>
2023-04-07 13:50:52 -0400

    test: fixed ng-add test

--- packages/style-lib/src/generators/ng-add/generator.spec.ts
+++ packages/style-lib/src/generators/ng-add/generator.spec.ts
@@ -1,20 +1,31 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Tree, readProjectConfiguration } from '@nrwl/devkit';
-
+import { libraryGenerator } from '@nrwl/workspace/generators';
+import { applicationGenerator } from '@nrwl/angular/generators';
+import { callRule } from '@nrwl/workspace/testing';
 import generator from './generator';
 import { NgAddGeneratorSchema } from './schema';
+import initGenerator from '../init/generator';
 
 describe('ng-add generator', () => {
   let appTree: Tree;
-  const options: NgAddGeneratorSchema = { name: 'test' };
+  const options: NgAddGeneratorSchema = {
+    angularApplication: 'client',
+    styleLibrary: 'style',
+  };
 
-  beforeEach(() => {
+  beforeAll(async () => {
     appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+
+    await applicationGenerator(appTree, {
+      name: 'client',
+    });
+    await initGenerator(appTree, { name: 'style' });
   });
 
   it('should run successfully', async () => {
     await generator(appTree, options);
-    const config = readProjectConfiguration(appTree, 'test');
+    const config = readProjectConfiguration(appTree, 'style');
     expect(config).toBeDefined();
   });
 });
