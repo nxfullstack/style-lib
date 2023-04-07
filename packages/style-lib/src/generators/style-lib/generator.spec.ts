5cc7d152bdc5d1d6679945c704e711ca98a17f4e
wgd3 <wallace.daniel3@gmail.com>
2023-04-07 08:09:12 -0400

    Initial commit

---
+++ packages/style-lib/src/generators/style-lib/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from "@nrwl/devkit/testing";
+import { Tree, readProjectConfiguration } from "@nrwl/devkit";
+
+import generator from "./generator";
+import { StyleLibGeneratorSchema } from "./schema";
+
+describe("style-lib generator", () => {
+  let appTree: Tree;
+  const options: StyleLibGeneratorSchema = { name: "test" };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace({ layout: "apps-libs" });
+  });
+
+  it("should run successfully", async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, "test");
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/style-lib/src/generators/style-lib/schema.d.ts
@@ -0,0 +1,5 @@
+export interface StyleLibGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
